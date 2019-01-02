6ca2574e827323b7b63cb1448b853b431eb4ec63
AnshKapoor <ansh605@gmail.com>
2019-01-02 23:38:49 +0530

    Added profile view button

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,13 @@
 function RemoveClasses(){
 $("#fff").remove();
 $("#ffy").remove();
-  
-  
-
-
-    
+}
+function Create_Button(){
+var button = "<button>Click here to view my profile</button>";
+$("#header").append(button);
+$("#header button").addClass("button");
 }
 window.onload = function(){
 setTimeout(RemoveClasses,10000);
+setTimeout(Create_Button,12000);
 }
